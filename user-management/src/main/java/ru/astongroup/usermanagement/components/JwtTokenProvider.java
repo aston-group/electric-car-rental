3a725bda8060f416930dcdc2c0ff748ebe8594c5
Andrew Muhlynin <rx.081@hotmail.com>
2025-02-18 14:03:49 +0500

    Добавлен файл JwtAuthenticationFilter

--- user-management/src/main/java/ru/astongroup/usermanagement/components/JwtTokenProvider.java
+++ user-management/src/main/java/ru/astongroup/usermanagement/components/JwtTokenProvider.java
@@ -4,10 +4,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
-import ru.astongroup.usermanagement.models.UserModel;
 
 import java.util.Date;
 import java.util.function.Function;
+
+import ru.astongroup.usermanagement.models.UserModel;
 
 @Component
 public class JwtTokenProvider {
@@ -15,28 +16,25 @@ public class JwtTokenProvider {
     private final String JWT_SECRET = "yourSecretKeyShouldBeLongerAndMoreComplex";
     private final long JWT_EXPIRATION_MS = 1000 * 60 * 60 * 24; // 24 hours in milliseconds
 
-    // Метод для получения JWT из строки токена
     public String getToken(UserModel userDetails) {
         return Jwts.builder()
-                .setSubject(userDetails.getUsername())
+                .setSubject(userDetails.getEmail())
+                .claim("roles", userDetails.getUserStatus().toString())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_MS))
-                .signWith(SignatureAlgorithm.HS256, JWT_SECRET)
+                .signWith(SignatureAlgorithm.HS256, JWT_SECRET.getBytes())
                 .compact();
     }
 
-    // Метод для получения имени пользователя из JWT
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
-    // Общий метод для получения информации из JWT
     public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
 
-    // Метод для получения всех утверждений (claims) из JWT
     private Claims getAllClaimsFromToken(String token) {
 
         try {
@@ -49,18 +47,15 @@ public class JwtTokenProvider {
         }
     }
 
-    // Метод для проверки, не истек ли JWT
     public boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
 
-    // Метод для получения даты истечения срока действия из JWT
     public Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
 
-    // Метод для проверки валидности JWT
     public boolean validateToken(String token, UserModel userDetails) {
         final String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));

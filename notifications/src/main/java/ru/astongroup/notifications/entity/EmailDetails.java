5cd459245ad220663b887e02ea052553173d36ad
gigaden <gigaden@ya.ru>
2025-02-21 19:49:59 +0300

    добавлены тесты для контроллера

--- notifications/src/main/java/ru/astongroup/notifications/entity/EmailDetails.java
+++ notifications/src/main/java/ru/astongroup/notifications/entity/EmailDetails.java
@@ -2,10 +2,12 @@ package ru.astongroup.notifications.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
--- notifications/src/main/java/ru/astongroup/notifications/entity/Notification.java
+++ notifications/src/main/java/ru/astongroup/notifications/entity/Notification.java
@@ -11,14 +11,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "notifications")
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
